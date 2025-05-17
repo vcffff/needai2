83744757849670edc1beca255b0c814b336dcef1
BATU994 <batu.beksultan@gmail.com>
2025-05-17 23:34:48 +0500

    books listststststw

--- lib/presentation/screens/books_/bookList/booksList.dart
+++ lib/presentation/screens/books_/bookList/booksList.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:needai/presentation/screens/books_/bookList/bookWebPage.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class bookList extends StatefulWidget {
   final String bookType;
@@ -13,6 +14,7 @@ class bookList extends StatefulWidget {
 
 class _bookListState extends State<bookList> {
   List<dynamic> books = [];
+  List<dynamic> filteredBooks = [];
   @override
   void initState() {
     super.initState();
@@ -20,48 +22,98 @@ class _bookListState extends State<bookList> {
   }
 
   Future<void> fetchBooks() async {
-    final DocumentReference documentReference = FirebaseFirestore.instance
-        .collection('books')
-        .doc('OK4Xx3TE6ovk0ODbfNqt');
+    try {
+      final DocumentReference documentReference = FirebaseFirestore.instance
+          .collection('books')
+          .doc('OK4Xx3TE6ovk0ODbfNqt');
 
-    final DocumentSnapshot snapshot = await documentReference.get();
-    if (snapshot.exists) {
-      setState(() {
-        books = snapshot.get('books');
-      });
-      for (var book in books) {
-        final String title = book['title'];
-        final String url = book['url'];
-        print("Title: $title, URL: $url");
+      final DocumentSnapshot snapshot = await documentReference.get();
+      if (snapshot.exists) {
+        setState(() {
+          books = snapshot.get('books');
+        });
+        filteredBooks =
+            books
+                .where((book) => book['type'] == widget.bookType.toString())
+                .toList();
+        for (var book in filteredBooks) {
+          final String title = book['title'];
+          final String url = book['url'];
+          final String type = book['type'];
+        }
+        print(filteredBooks);
+      } else {
+        print("No books found.");
       }
-    } else {
-      print("No books found.");
+    } catch (e) {
+      print(e.toString());
     }
   }
 
   Widget build(BuildContext context) {
-    return Container(
-      child: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 2.0,
-          mainAxisSpacing: 2.0,
-        ),
-        itemCount: books.length,
-        itemBuilder: (context, index) {
-          ConstrainedBox(
-            constraints: BoxConstraints(
-              maxWidth: MediaQuery.of(context).size.width * 0.3,
-              minHeight: MediaQuery.of(context).size.width * 0.3,
-            ),
-            child: InkWell(
-              onTap: () {},
-              child: Container(
-                child: Column(children: [Text(books[index]['title'])]),
+    return Scaffold(
+      appBar: AppBar(title: const Text("Filtered Books")),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 8.0,
+            mainAxisSpacing: 8.0,
+            childAspectRatio: 0.75,
+          ),
+          itemCount: filteredBooks.length,
+          itemBuilder: (context, index) {
+            return ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width * 0.45,
+                minHeight: MediaQuery.of(context).size.width * 0.3,
               ),
-            ),
-          );
-        },
+              child: InkWell(
+                onTap: () async {
+                  final Uri bookUri = Uri.parse(filteredBooks[index]['url']);
+                  if (await canLaunchUrl(bookUri)) {
+                    try {
+                      await launchUrl(bookUri);
+                    } catch (e) {
+                      print(e.toString());
+                    }
+                  }
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.book, size: 40, color: Colors.blue),
+                      const SizedBox(height: 10),
+                      Text(
+                        filteredBooks[index]['title'],
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        filteredBooks[index]['type'],
+                        style: const TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
--- lib/presentation/screens/books_/books.dart
+++ lib/presentation/screens/books_/books.dart
@@ -36,43 +36,51 @@ class TypeOfBooks extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<String>>(
-      future: fetchTypes(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          return Center(child: Text('Error: ${snapshot.error}'));
-        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-          return const Center(child: Text('No book types found.'));
-        } else {
-          final typesOfBooks = snapshot.data!;
-          return ListView.builder(
-            itemCount: typesOfBooks.length,
-            itemBuilder: (context, i) {
-              return Card(
-                elevation: 2.0,
-                margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                child: ListTile(
-                  onTap: () {
-                    print(typesOfBooks[i]);
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder:
-                            (context) => bookList(bookType: typesOfBooks[i]),
-                      ),
-                    );
-                  },
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 10),
-                  leading: const Icon(Icons.book, color: Colors.black),
-                  title: Text(typesOfBooks[i]),
-                ),
-              );
-            },
-          );
-        }
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Available Books', style: TextStyle(fontSize: 20)),
+      ),
+      body: FutureBuilder<List<String>>(
+        future: fetchTypes(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No book types found.'));
+          } else {
+            final typesOfBooks = snapshot.data!;
+            return ListView.builder(
+              itemCount: typesOfBooks.length,
+              itemBuilder: (context, i) {
+                return Card(
+                  elevation: 2.0,
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 8,
+                    vertical: 4,
+                  ),
+                  child: ListTile(
+                    onTap: () {
+                      print(typesOfBooks[i]);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder:
+                              (context) => bookList(bookType: typesOfBooks[i]),
+                        ),
+                      );
+                    },
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+                    leading: const Icon(Icons.book, color: Colors.black),
+                    title: Text(typesOfBooks[i]),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
